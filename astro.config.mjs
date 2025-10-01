8877b88404ab1a0907170a9c7b7bac3a31b5582e
Ваше имя <@email.com>
2025-10-01 15:50:04 +0300

    Add: favicon.svg, favicon.png, robots.txt and improve sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,19 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://bookscalculator.ru',
+  site: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://bookscalculator.ru',
   compressHTML: true,
 
   build: {
     inlineStylesheets: 'auto'
   },
 
-  integrations: [tailwind(), sitemap()]
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ]
 });
